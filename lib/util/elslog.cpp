f9620552684873f9a4638df9e646cdc7c367f3f0
me <me_xeon_wsl_ubuntu18@brng.us>
2023-03-19 16:09:11 -0700

    missing lib files

---
+++ lib/util/elslog.cpp
@@ -0,0 +1,58 @@
+#include "elslog.h"
+#include <Arduino.h>
+
+
+#ifdef PIO_UNIT_TESTING
+
+#else
+#include "../../src/src/web.h"
+#endif
+
+Log::Msg el;
+
+void Log::Msg::maybeSend(){
+    #ifndef PIO_UNIT_TESTING
+    sendLogP(this);
+    #endif
+    #ifdef PIO_UNIT_TESTING
+    //printf("mock send");
+    #endif
+}
+
+void Log::Msg::error(){
+    if(this->t == T::WS){
+        printf("\nWS Log: %s\n",this->buf);
+        maybeSend();
+    }
+}
+
+void Log::Msg::error(const char* s){
+    if(this->t == T::WS){
+        sprintf(buf,s);
+        maybeSend();
+        printf(s);
+    }
+}
+void Log::Msg::halt(const char* s){
+    if(this->t == T::WS){
+        sprintf(buf,s);
+        maybeSend();
+        printf(s);
+    }
+}
+
+#ifndef PIO_UNIT_TESTING
+
+void Log::Msg::errorTaskImpl(void* _this){
+    static_cast<Log::Msg *>(_this)->error();
+    vTaskDelete(NULL);
+}
+void Log::Msg::errorTask(){
+    xTaskCreate(this->errorTaskImpl,"errorTask",2048,this, 5,NULL);
+}
+
+#endif
+
+void Log::Msg::addMsg(const char* s){
+    sprintf(buf,s);
+}
---
+++ lib/util/elslog.h
@@ -0,0 +1,40 @@
+#pragma once
+//#include <Arduino.h>
+
+//#include "config.h"
+
+#ifndef PIO_UNIT_TESTING
+//#include "../../src/src/config.h"
+//#include "../../src/src/web.h"
+#endif
+
+namespace Log{
+    enum class T {
+            WEB,
+            SER,
+            WS
+        };
+    enum class Level {
+        ERROR,
+        WARN,
+        INFO,
+        HALT
+    };
+    class Msg{
+        public:
+        T t = T::WS;
+        bool hasError = false;
+        char buf[500] = "";
+        Level level = Level::ERROR;
+        void maybeSend();
+        void error(void);
+        void error(const char*);
+        void halt(const char*);
+        void addMsg(const char*);
+        void errorTask();
+        static void errorTaskImpl(void* _this);
+    };
+
+}
+
+extern Log::Msg el;
